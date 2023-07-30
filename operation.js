6b8eb14e4da1d0c3dd0efacc4ef26dacf0ac942c
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-07-31 00:25:48 +0600

    Added

---
+++ operation.js
@@ -0,0 +1,23 @@
+//addition
+var onionPrice=50;
+var eggPrice=20; 
+var totalPrice= onionPrice + eggPrice;
+console.log(totalPrice);
+
+//subtraction
+var onionPrice=50;
+var eggPrice=20; 
+var bakiPrice= onionPrice - eggPrice;
+console.log(bakiPrice);
+
+//multiplaction
+var onionPrice=50;
+var eggPrice=20; 
+var totalPrice1= onionPrice * eggPrice;
+console.log(totalPrice1);
+
+//division
+var onionPrice=50;
+var eggPrice=20; 
+var totalPrice2= onionPrice / eggPrice;
+console.log(totalPrice2);
---
+++ shorthand.js
@@ -0,0 +1,24 @@
+//variable declaration
+var tomatoPrice=30;
+
+//value updated
+tomatoPrice=45;
+
+//add some value
+var price1=100;
+
+//add some value to the excisting value
+price1=price1+20;
+console.log(price1);
+
+//addition tp the shorthand
+price1 += 10;
+price1 -=10;
+price1 *=10;
+price1 /=10;
+
+//increment
+price1 ++; //+1
+
+//decrement
+price1--; //-1
