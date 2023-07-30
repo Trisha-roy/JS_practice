c9e63846ac0a122660bc96b2e0f2f4eefc9aace4
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-07-31 01:06:00 +0600

    added

---
+++ problem-solving.js
@@ -0,0 +1,8 @@
+var mother=1000;
+var apple=300;
+var orange=400;
+
+var self=apple+orange;
+
+var nijerKaseAse=mother-self;
+console.log(nijerKaseAse);
---
+++ type.js
@@ -0,0 +1,21 @@
+var price=10;
+// var price='10';
+console.log(typeof price)
+
+//undefined
+var roomantic;
+console.log(typeof roomantic);
+
+//problem of javascript
+var p1=0.1;
+var p2=0.2;
+sum=p1+p2
+console.log(sum);
+
+//solution of this problem
+var num1=0.1;
+var num2=0.2;
+sum1=num1+num2;
+sum1= sum1.toFixed(1);
+sum1=parseFloat(sum1)
+console.log(sum1)
