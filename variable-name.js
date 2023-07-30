d07452d35a51c7dfacbc680b5c2114fa10037b79
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-07-30 21:52:13 +0600

    Added something

--- variable-name.js
+++ variable-name.js
@@ -1,1 +1,18 @@
-//variable name can not be any keywords
+//1.variable name can not be any keywords
+// var false=false
+
+//2. variable must be onhe word
+
+//3. Variable name cant be quotation
+
+//4.Numer cannot be starts with a number but ends with a number
+
+//5.for long name variable cant use dash(-) but use a underscore (_)
+
+//6.How to use long name in variable
+
+var  user_name="Trisha Roy";
+var usercurrentaddress="sylhet";
+var user_current_address="sylhet";//snake case
+var currentAddress="8 no house , Gulapganj house, Sylhet sadar";//camel case
+var CurrentAddress="8 no house , Gulapganj house, Sylhet sadar";//pascal case
