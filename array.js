3d714090c0bc22a91aeccc57a32767bc9c25ab4a
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-07-31 22:03:19 +0600

    Added array.js

---
+++ array.js
@@ -0,0 +1,15 @@
+var actress=["mahi","koel","shovosree"];
+console.log(actress);
+
+console.log(actress[1]);
+
+var positionOfIndex=actress.indexOf(188);
+console.log(positionOfIndex);
+
+//push
+actress.push("Trisha");
+console.log(actress);
+
+//pop
+actress.pop()
+console.log(actress)
