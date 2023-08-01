b00dfcd278580195904f315a31a9a7e14af1cd8e
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-01 21:59:53 +0600

    Added conditional.js

---
+++ conditional.js
@@ -0,0 +1,18 @@
+var iphone=95000;
+var myBudget=15000;
+
+if(iphone<myBudget)
+{
+    console.log("I buy this")
+    
+}else{
+    console.log("i Didn't buy iphone because my phone is over price");
+}
+
+var graduated=false;
+var salary=75000;
+if(graduated=true && salary>85000){
+    console.log("Acceptable")
+}else{
+    console.log("Not acceptable")
+}
