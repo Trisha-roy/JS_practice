2b9493798515e4da0bb17a6f02b5cce038233107
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-02 21:30:55 +0600

    Added some file

---
+++ loop.js
@@ -0,0 +1,34 @@
+//while Loop
+var roastGiven=0;
+while(roastGiven<5){
+    console.log("Roast khabo");
+    console.log(roastGiven);
+    roastGiven++;
+}
+console.log("Number while loop")
+var number=0;
+while(number<=10){
+    console.log(number);
+    number++;
+}
+
+//1 to 10 even numbers
+var even=0;
+while(even<=20){
+    console.log("even number: ")
+    console.log(even);
+    even=even+2;
+}
+
+//odd number 1 to 100
+var odd=1;
+while(odd<=100){
+    console.log("Odd number: ");
+    console.log(odd);
+    odd=odd+2;
+}
+
+//for Loop
+for(i=0;i<5;i++){
+    console.log(i)
+}
