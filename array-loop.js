9b4fb91df747ad71f932dbcd3f8f99de289406e9
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-02 21:56:12 +0600

    Added some files

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+var array=[45,87,95,66,73,34,90];
+
+for (i=0;i<array.length;i++){
+    var number=array[i]
+    console.log(number)
+}
+var numbers=[20,30,40,50,60,70,80,90,100];
+
+for(i=0;i<numbers.length;i++){
+    var Num=numbers[i]
+    console.log(Num)
+}
