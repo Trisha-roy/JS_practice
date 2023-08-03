7f8af915f586bbf4e8463bc155081b94aeb236f8
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-04 00:54:10 +0600

    Function created

--- js-function.js
+++ js-function.js
@@ -1,3 +1,20 @@
+//function declaration
 function startFan(){
     console.log("Stand Up!")
 }
+startFan();
+
+//declare a parameter
+function singara(money){
+    console.log("Singara kinte chai");
+    console.log(money)
+}
+
+singara(250);
+
+function sum(a,b,c,d){
+    console.log(a,b,c,d);
+    var sum=a+b+c+d;
+    console.log(sum)
+}
+sum(10,20,30,40);
