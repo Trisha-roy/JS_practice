2eeb3ce9a4cdcf946fc7f282b664b14f7c19210a
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-04 21:38:41 +0600

    Added objects

---
+++ objects.js
@@ -0,0 +1,9 @@
+var student={
+    id:115,
+    Name:"Trisha",
+    Dept:"CSE"
+}
+console.log(student);
+console.log(student.id);
+student.id=173;
+console.log(student.id)
--- return.js
+++ return.js
@@ -13,4 +13,18 @@ function bringSingara(money){
     return quantity;
 }
 var givenSingara=bringSingara(200);
-console.log('Singara given : ',givenSingara)
+console.log('Singara given : ',givenSingara);
+
+//function functionsName(parameter){
+    // function body
+    // return
+// }
+// var returnedValue=functionsName(parameters Value)
+
+function getAverage(assignment1,assignment2,assignment3){
+    console.log('My assignment mark is : ',assignment1,assignment2,assignment3)
+    var mark=((assignment1+assignment2+assignment3)/3);
+    return mark;
+}
+var averageMark=getAverage(48,51,53);
+console.log(averageMark)
