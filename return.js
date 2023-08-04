5f379b19bd2ac8d34fd0c54adeea96ea763a36a0
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-04 21:09:33 +0600

    Added return Funtion

---
+++ return.js
@@ -0,0 +1,16 @@
+// function add(number1,number2){
+//     console.log(number1,number2);
+//     var sum=number1+number2;
+//     console.log(sum)
+//     return 15;
+// }
+// var total=add(15,15);
+// console.log('total',total)
+
+function bringSingara(money){
+    var singaraPrice=5;
+    quantity=money/singaraPrice
+    return quantity;
+}
+var givenSingara=bringSingara(200);
+console.log('Singara given : ',givenSingara)
