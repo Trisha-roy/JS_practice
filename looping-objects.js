d498ea9e44c728c837a057ea5dbda185373023dc
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-04 22:24:58 +0600

    Objecs created

--- looping-objects.js
+++ looping-objects.js
@@ -4,3 +4,25 @@ var shoppingCart={
     dress:104,
     pant:34
 }
+console.log(shoppingCart)
+
+var keys=Object.keys(shoppingCart)
+console.log(keys);
+
+var values=Object.values(shoppingCart)
+console.log(values);
+
+//looping objects
+for (var i=0;i<keys.length;i++){
+    var propertiesName=keys[i];
+    var propertiesValues=shoppingCart[propertiesName]
+
+    console.log(propertiesName)
+    console.log(propertiesValues)
+}
+
+//for in loop
+for(var propertiesName in shoppingCart){
+    console.log(propertiesName)
+}
+
