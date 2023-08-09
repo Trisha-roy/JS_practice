881968c922f8884eebccc9aa0a74d9ae612b48c0
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-09 20:39:20 +0600

    Updated Something

---
+++ search.js
@@ -0,0 +1,4 @@
+const lyrics="Ami abar klanto pothochari katar agun laage vari jani jibone thambe dujonare kemon jeno alada alada lage";
+
+const doesExist=lyrics.includes('pakji');
+console.log(doesExist)
