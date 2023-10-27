68d72dddadb76f35d655846aa4a1dd8d99955222
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-10-27 01:21:36 -0300

    Updated Storage

--- Es6-intro/storage.js
+++ Es6-intro/storage.js
@@ -1,1 +1,14 @@
-localStorage.setItem('userId',173115)
+const addToLocalStorage=()=>{
+    const idInput=document.getElementById('storage-id');
+    const id=idInput.value;
+
+    const valueInput=document.getElementById('storage-value');
+    const value=valueInput.value;
+
+    localStorage.setItem(id,value);
+    idInput.value='';
+    valueInput.value=''
+}
+const name = 'Hero';
+const age = 34;
+const person = {name, age}; console.log(person);
